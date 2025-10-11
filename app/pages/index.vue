82847d0d86cacba0389845473050e605ce58d37a
tropikow <jovannypersonal@gmail.com>
2025-10-11 14:26:38 -0500

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const isAuthenticated = ref<boolean>(false)
+  const router = useRouter()
+  if(isAuthenticated.value) {
+    router.push('/main')
+  } else {
+    router.push('/auth')
+  }
+</script>
