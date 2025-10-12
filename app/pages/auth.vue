743bbac5e3f9f9d660c5cc53710648f4ef44d9b0
tropikow <jovannypersonal@gmail.com>
2025-10-12 02:02:30 -0500

    Enhance auth page layout and styles: add header for sign-in, adjust login container dimensions, and restore box-sizing rules in CSS.

--- app/pages/auth.vue
+++ app/pages/auth.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="background">
+    <h2>Sign in to ConnectCALL</h2>
     <form class="loginContainer">
       <input type="email" placeholder="Email address" />
+      <hr />
       <input type="password" placeholder="Password" />
     </form>
   </div>
 </template>
-<style>
+<style scoped>
   .background {
     display: flex;
-    justify-content: center;    
+    justify-content: center;
+    flex-direction: column;
     align-items: center;
     width: 100%;
     height: 100%;    
   }
   .loginContainer {
+    display: flex;
+    flex-direction: column;
     border: 1px solid var(--colorGrayLight);
     border-radius: var(--borderRadius);            
     overflow: hidden;    
-  }
+    width: 500px;
+    height: 100px;    
+  }    
   .loginContainer>input {
     width: 100%;
-    height: 100%;    
+    height: 100%;
+    border: none;
+    font-size: 16px;    
+    background-color: transparent;
+  }
+  hr {    
+    border: 1px solid var(--colorGrayLight);
+    margin: 0;
+    flex-shrink: 0;
+  }
+  h2 {
+    font-weight: bold;
   }
 </style>
