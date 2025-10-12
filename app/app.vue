8722a8e105186a4e4d06ba3a58e8be05e8ded49c
tropikow <jovannypersonal@gmail.com>
2025-10-12 00:48:15 -0500

    Refactor app layout and styles: change root element from div to body, add CSS variables for colors, and enhance auth page with a centered login form.

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <body>
     <NuxtPage />
-  </div>
+  </body>
 </template>
--- app/pages/auth.vue
+++ app/pages/auth.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    auth
+  <div class="background">
+    <form class="loginContainer">
+      <input type="email" placeholder="Email address" />
+      <input type="password" placeholder="Password" />
+    </form>
   </div>
 </template>
+<style>
+  .background {
+    display: flex;
+    justify-content: center;    
+    align-items: center;
+    width: 100%;
+    height: 100%;    
+  }
+  .loginContainer {
+    border: 1px solid var(--colorGrayLight);
+    border-radius: var(--borderRadius);            
+    overflow: hidden;    
+  }
+  .loginContainer>input {
+    width: 100%;
+    height: 100%;    
+  }
+</style>
